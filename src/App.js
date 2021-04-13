be7fd7d83ca3d3c43b15a8ebbab0d1962d566ea1
Mike Voynovich <mvoyno@gmail.com>
2021-04-13 14:12:46 -0400

    updated basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Resume from "./pages/resume";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
     <div>
       <Switch>
         <Route exact path={["/", "/about"]}>
